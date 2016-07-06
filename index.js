cbb7da04448b63e6d9ebf3f8114521b7ac66e723
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-07-06 13:47:34 -0400

    clean up code

--- index.js
+++ index.js
@@ -75,29 +75,6 @@ app.post('/signup', function(req, res) {
       .catch(function(err) {
         res.status(400).json({ "status": "fail", "message": err.message });
       });
-
-    // User.findOne({ _id: username }, function(err, user) {
-    //   if (err) {
-    //     res.status(400).json({ "status": "fail", "message": err.message });
-    //     return;
-    //   }
-    //   if (!user) {
-    //     // create user
-    //     User.create({
-    //       _id: username,
-    //       password: encryptedPassword
-    //     }, function(err) {
-    //       if (err) {
-    //         res.status(400).json({ "status": "fail", "message": err.message });
-    //         return;
-    //       }
-    //       res.status(200).json({ "status": "ok" });
-    //     });
-    //   } else {
-    //     // user already exists, json 409
-    //     res.status(409).json({ "status": "fail", "message": "Username is taken" });
-    //   }
-    // });
   });
 });
 
@@ -141,44 +118,6 @@ app.post('/login', function(req, res) {
     .catch(function(err) {
       res.status(400).json({ "status": "fail", "message": "Error finding user " + err.message });
     });
-
-  // User.findOne({ _id: username }, function(err, user) {
-  //   if (err) {
-  //     res.status(400).json({ "status": "fail", "message": "Error finding user " + err.message });
-  //     return;
-  //   }
-    // // if user isn't found
-    // if (!user) {
-    //   res.status(400).json({ "status": "fail", "message": "User not found" });
-    //   return;
-    // } else {
-    //   // compare submitted password with encrypted password in databse
-    //   bcrypt.compare(password, user.password, function(err, matched) {
-    //     if (err) {
-    //       res.status(400).json({ "status": "fail", "message": "Error in bcrypt: " + err.message });
-    //       return;
-    //     }
-    //     // if passwords match, generate token and push to users token array
-    //     if (matched) {
-    //       var token = randtoken.generate(64);
-    //       // set token to expire in 10 days and push to authenticationTokens array
-    //       user.authenticationTokens.push({ token: token, expiration:  Date.now() + 1000 * 60 * 60 * 24 * 10 });
-    //       // save user's new token
-    //       user.save(function(err) {
-    //         if (err) {
-    //           res.status(400).json({ "status": "fail", "message": "Error saving token: " + err.message });
-    //           return;
-    //         }
-    //         // return token in response body
-    //         res.status(200).json({ "status": "ok", "token": token });
-    //       });
-    //     } else {
-    //       // incorrect password
-    //       res.status(400).json({ "status": "fail", "message": "Password doesn't match" });
-    //     }
-    //   });
-    // }
-  // });
 });
 
 // allows users to order coffee, charges purchases with stripe
@@ -200,18 +139,6 @@ app.post('/orders', authRequired, function(req, res) {
       }
       res.status(400).json({ "status": "fail", "message": errorMessage });
     });
-  // function(err) {
-  //   if (err) {
-  //     // construct a more readable error message
-  //     var errorMessage = "";
-  //     for (var key in err.errors) {
-  //       errorMessage += err.errors[key].message + " ";
-  //     }
-  //     res.status(400).json({ "status": "fail", "message": errorMessage });
-  //     return;
-  //   }
-  //   res.status(200).json({ "status": "ok" });
-  // });
 });
 
 // returns all orders the user has previously submitted
@@ -244,25 +171,6 @@ function authRequired(req, res, next) {
       //if there was an error finding the user by authenticationToken
       res.status(400).json({ "status": "fail", "message": err.errors });
     });
-
-
-  // User.findOne(
-  //   //check if token exists and hasn't expired
-  //   { authenticationTokens: { $elemMatch: { token: token, expiration: { $gt: Date.now() } } } },
-  //   function(err, user) {
-  //     //if there was an error finding the user by authenticationToken
-  //     if (err) {
-  //       res.status(400).json({ "status": "fail", "message": err.errors });
-  //       return;
-  //     }
-  //     if (user) {
-  //       req.user = user;
-  //       next();
-  //     } else {
-  //       res.status(400).json({ "status": "fail", "message": "Session expired. Please sign in again." });
-  //       return;
-  //     }
-  //   });
 }
 
 app.listen(3000, function() {
