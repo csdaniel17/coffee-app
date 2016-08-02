639349ffbf4583cc939595eb8ff2b8d66900b75d
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-08-02 09:31:00 -0400

    update redirect

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -29,6 +29,9 @@ app.config(function($routeProvider){
     .when('/thankyou', {
       controller: 'PaymentController',
       templateUrl: '/thankyou.html'
+    })
+    .otherwise({
+      redirectTo: '/'
     });
 });
 
